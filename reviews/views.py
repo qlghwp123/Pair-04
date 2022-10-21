48616bb2426b878b623dc5537731909bbd8a0f22
qlghwp123 <qlghwp123@naver.com>
2022-10-21 14:02:35 +0900

    Add : incomplete reviews/update code

--- reviews/views.py
+++ reviews/views.py
@@ -36,3 +36,25 @@ def detail(req, review_pk):
     data = Review.objects.get(id=review_pk)
 
     return render(req, 'reviews/detail.html', {'data': data})
+
+
+@login_required
+def update(req, review_pk):
+    data = Review.objects.get(id=review_pk)
+
+    if req.method == 'POST':
+        form = ReviewForm(req.POST, instance=data)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('reviews:detail')
+
+    else:
+        form = ReviewForm(instance=data)
+
+    context = {
+        'form': form
+    }
+
+    return render(req, 'reviews/create.html', context)
