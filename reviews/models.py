a9802f65df59c4f73520170ad6752de4f9e20502
qlghwp123 <qlghwp123@naver.com>
2022-10-21 10:23:18 +0900

    Add : reviews/create and url setting

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
+class Review(models.Model):
+    user = models.CharField(max_length=20)
+    title = models.CharField(max_length=50)
+    content = models.TextField()
+    movie_name = models.CharField(max_length=30)
+    grade = models.FloatField(validators=[MinValueValidator(0.0),MaxValueValidator(5.0)])
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import ReviewForm
 
 # Create your views here.
+def create(req):
+    if req.method == 'POST':
+        form = ReviewForm(req.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('reviews:index')
+
+    else:
+        form = ReviewForm()
+
+    context = {
+        'form': form
+    }
+
+    return render(req, 'reviews/create.html', context)
